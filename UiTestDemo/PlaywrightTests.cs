116fb728f84ce20a8ba207235b52894d1b454bc9
Lassi Autio <lassi.autio@solita.fi>
2023-08-28 21:46:51 +0300

    Add comments

--- UiTestDemo/PlaywrightTests.cs
+++ UiTestDemo/PlaywrightTests.cs
@@ -1,9 +1,12 @@
-﻿using System.Text.RegularExpressions;
-using Microsoft.Playwright;
+﻿using Microsoft.Playwright;
 using Xunit;
 
 namespace PlaywrightTests;
 
+/// <summary>
+/// This class is based on the Playwright example at https://playwright.dev/dotnet/docs/intro
+/// but for xUnit test framework.
+/// </summary>
 public class PlayWrightTests : IAsyncLifetime, IDisposable
 {
     private IPlaywright _playwright;
@@ -13,24 +16,33 @@ public class PlayWrightTests : IAsyncLifetime, IDisposable
     public async Task InitializeAsync()
     {
         _playwright = await Playwright.CreateAsync();
-        _browser = await _playwright.Chromium.LaunchAsync();
+        _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
         _page = await _browser.NewPageAsync();
     }
 
+    // Focus on this method, it is the test.
     [Fact]
     public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
     {
         await _page.GotoAsync("https://playwright.dev");
 
+        // Check that the title contains "Playwright".
+        Thread.Sleep(TimeSpan.FromSeconds(5));
         Assert.Contains("Playwright", await _page.TitleAsync());
 
+        // Find the "Get started" link (or button).
         var getStarted = _page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
 
+        // Check that the link points to the intro page.
+        Thread.Sleep(TimeSpan.FromSeconds(5));
         Assert.Equal("/docs/intro", await getStarted.GetAttributeAsync("href"));
 
+        // Click the link.
         await getStarted.ClickAsync();
 
-        Assert.Matches(new Regex(".*intro"), _page.Url);
+        // Check that the URL contains "intro".
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+        Assert.Contains("intro", _page.Url);
     }
 
     public async Task DisposeAsync()
